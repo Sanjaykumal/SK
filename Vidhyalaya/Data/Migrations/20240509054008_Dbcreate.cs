bc66d7e450184ef481945044a9f8e07b8d31148e
Sanjay <sk5248916@gmail.com>
2024-05-09 12:27:34 +0545

    Added migration folder

---
+++ Vidhyalaya/Data/Migrations/20240509054008_Dbcreate.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vidhyalaya.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Dbcreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Grades",
+                columns: table => new
+                {
+                    Label = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ClassTeacher = table.Column<string>(type: "TEXT", nullable: false),
+                    Medium = table.Column<string>(type: "TEXT", nullable: false),
+                    Subjects = table.Column<string>(type: "TEXT", nullable: false),
+                    SessionYear = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Grades", x => x.Label);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Address = table.Column<string>(type: "TEXT", nullable: false),
+                    sex = table.Column<int>(type: "INTEGER", nullable: false),
+                    Dob = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Photo = table.Column<byte[]>(type: "BLOB", nullable: false),
+                    Guardian_name = table.Column<string>(type: "TEXT", nullable: false),
+                    Guardian_contact = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Grades");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
---
+++ Vidhyalaya/Data/Migrations/20240509054206_FKStudent.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vidhyalaya.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FKStudent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GradeLabel",
+                table: "Students",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_GradeLabel",
+                table: "Students",
+                column: "GradeLabel");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Students_Grades_GradeLabel",
+                table: "Students",
+                column: "GradeLabel",
+                principalTable: "Grades",
+                principalColumn: "Label",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Students_Grades_GradeLabel",
+                table: "Students");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Students_GradeLabel",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "GradeLabel",
+                table: "Students");
+        }
+    }
+}
