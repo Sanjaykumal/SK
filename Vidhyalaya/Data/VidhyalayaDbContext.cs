a4cb8c43e5a2a44542f475b6688783ca84e1876a
Sanjay <sk5248916@gmail.com>
2024-05-09 12:29:31 +0545

    configured db context

---
+++ Vidhyalaya/Data/VidhyalayaDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+public class VidhyalayaDbContext : DbContext
+{
+    public DbSet<Grade> Grades { get; set; }
+    public DbSet<Student> Students { get; set; }
+
+     protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Grade>().HasKey(g => g.Label);
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=Vidhyalaya.db");
+    }
+}
+   
